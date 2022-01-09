23261671fd3cb904f9cae370f264f0ec979be686
Noah Saso <noahsaso@gmail.com>
2022-01-08 17:37:40 -0800

    connect to devices directly instead of trying to discover them

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from dotenv import load_dotenv
-from lifxlan import LifxLAN, light
+from lifxlan import LifxLAN, Light
 import os
+import asyncio
 
 app = Flask(__name__)
 lifx = LifxLAN()
 
 load_dotenv()
-NAMES = os.getenv("NAMES", "").split(",")
+DEVICES = os.getenv("DEVICES", "").split(",")
 
 # [hue (0-65535), saturation (0-65535), brightness (0-65535), Kelvin (2500-9000)]
 COLORS = {
@@ -15,34 +16,43 @@ COLORS = {
 }
 
 
-def set_them_lights_and_return_missing(names, color):
-    print(f"Trying to set {names} to {color}...")
+def set_devices_and_return_missing(devices, color):
+    print(f"Trying to set {devices} to {color}...")
 
-    group = lifx.get_devices_by_name(NAMES)
+    success = []
+    missing = []
+    for device in devices:
+        [mac, ip] = device.split("-")
+        l = Light(mac, ip)
 
-    group.set_power(True)
-    group.set_color(color)
+        try:
+            # dont do rapid so we can catch error
+            l.set_power(True)
+            l.set_color(color, rapid=True)
+            success.append(device)
+        except:
+            missing.append(device)
 
-    light_names = set([d.get_label() for d in group.get_device_list()])
-    print(f"Set {light_names} to {color}")
+    print(f"Set {success} to {color}")
 
-    missing_light_names = [name for name in NAMES if name not in light_names]
-    return missing_light_names
+    return missing
 
 
 @app.route("/warm_dim")
 async def warm_dim():
-    missing_light_names = set_them_lights_and_return_missing(NAMES, COLORS["warm_dim"])
+    missing = set_devices_and_return_missing(DEVICES, COLORS["warm_dim"])
 
-    if len(missing_light_names) > 0:
+    if len(missing) > 0:
         # try to set color to missing lights 10 times
         for _ in range(10):
-            missing_light_names = set_them_lights_and_return_missing(
-                missing_light_names, COLORS["warm_dim"]
+            missing = set_devices_and_return_missing(
+                missing,
+                COLORS["warm_dim"],
             )
 
-            if len(missing_light_names) == 0:
+            if len(missing) == 0:
                 break
+            await asyncio.sleep(5)
 
     return "Done"
 
