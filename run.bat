389f420fdeb38324060991c62c5b2842bce95f33
Noah Saso <noahsaso@gmail.com>
2022-01-08 16:28:19 -0800

    changed windows batch to powershell script

--- run.bat
+++
@@ -1,3 +0,0 @@
-.\env\Scripts\Activate.ps1
-
-waitress-serve --host 0.0.0.0 --port=5439 server:app
---
+++ run.ps1
@@ -0,0 +1,5 @@
+Set-ExecutionPolicy Unrestricted
+
+.\env\Scripts\Activate.ps1
+
+waitress-serve --host 0.0.0.0 --port 5439 server:app
